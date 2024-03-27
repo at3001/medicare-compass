ee8416bc0477a449ed5cec7c1679c90bdd99328f
semma66 <semmas@seas.upenn.edu>
2024-03-27 14:41:34 -0400

    beginning

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
+/* NEED TO EDIT THIS!! JUST OUTLINE FOR NOW */
+
+
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import ClassPage from './pages/ClassPage';
+
+
+// App is the root component of our application and as children contain all our pages
+// We use React Router's BrowserRouter and Routes components to define the pages for
+// our application, with each Route component representing a page and the common
+// NavBar component allowing us to navigate between pages (with hyperlinks)
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
