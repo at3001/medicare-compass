c7a4bdbabe0590a585cbbcc1b0929a44b06eb82a
lillyguoupenn <lillyguo@wharton.upenn.edu>
2024-04-03 14:44:10 -0400

    changed js to bites

--- client (frontend)/src/pages/ClassPage.js
+++ client (frontend)/src/pages/ClassPage.js
@@ -3,6 +3,9 @@ const sentencesCn = document.querySelectorAll('#content-info-cn span');
 const continueBtn = document.getElementById('continue-btn');
 let currentIndex = 0;
 
+const bitesLeft = document.querySelectorAll('#bite-left');
+const bitesRight = document.querySelectorAll('#bite-right');
+
 function showNextSentence() {
     if (currentIndex < sentencesEng.length) {
         currentIndex++;
@@ -15,4 +18,17 @@ function showNextSentence() {
     }
 }
 
-continueBtn.addEventListener('click', showNextSentence);
+function showNextBite() {
+    if (currentIndex < bitesRight.length) {
+        currentIndex++;
+    }
+
+    for (let i = 0; i < currentIndex; i++) {
+        bitesLeft[i].classList.remove('hidden');
+        bitesRight[i].classList.remove('hidden');
+        // to do: add increase to progress bar
+    }
+}
+
+
+continueBtn.addEventListener('click', showNextBite);
