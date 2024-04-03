2a43284f5530b9c3a4480f5fbd058b98c1935314
lillyguoupenn <lillyguo@wharton.upenn.edu>
2024-04-03 00:37:16 -0400

    added intro paragraph

--- client (frontend)/src/pages/ClassPage.js
+++ client (frontend)/src/pages/ClassPage.js
@@ -1,14 +1,16 @@
-const sentences = document.querySelectorAll('#content-info span');
+const sentencesEng = document.querySelectorAll('#content-info-eng span');
+const sentencesCn = document.querySelectorAll('#content-info-cn span');
 const continueBtn = document.getElementById('continue-btn');
 let currentIndex = 0;
 
 function showNextSentence() {
-    if (currentIndex < sentences.length) {
+    if (currentIndex < sentencesEng.length) {
         currentIndex++;
     }
 
     for (let i = 0; i < currentIndex; i++) {
-        sentences[i].classList.remove('hidden');
+        sentencesEng[i].classList.remove('hidden');
+        sentencesCn[i].classList.remove('hidden');
     }
 }
 
