bebeecb885adf6661f9d185612bec2540491f7f9
lillyguoupenn <lillyguo@wharton.upenn.edu>
2024-04-03 23:09:37 -0400

    finished progress bar

--- client (frontend)/src/pages/ClassPage.js
+++ client (frontend)/src/pages/ClassPage.js
@@ -12,19 +12,24 @@ function showNextBite() {
     for (let i = 0; i < currentIndex; i++) {
         bitesLeft[i].classList.remove('hidden');
         bitesRight[i].classList.remove('hidden');
-        // to do: add increase to progress bar
     }
 
     // content container scrolls to bottom
     var elem = document.getElementsByClassName('content-container')[0];
     elem.scrollTop = elem.scrollHeight;
+
+    if (currentIndex === bitesLeft.length) {
+        continueBtn.textContent = "Next section";
+    }
 }
 
 function increaseProgress() {
     var progressBar = document.getElementById('progress-bar');
-    var currentWidth = progressBar.style.width || (100 / (bitesLeft.length + 1)) + '%';
+    var currentWidth = progressBar.style.width;
+    
     var newWidth = parseFloat(currentWidth) + (100 / (bitesLeft.length + 1)); // Increase width by 10%
     progressBar.style.width = newWidth + '%';
+    // progressBar.textContent = newWidth;
   }
 
 continueBtn.addEventListener('click', showNextBite);
